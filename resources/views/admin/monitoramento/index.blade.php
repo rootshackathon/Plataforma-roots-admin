d0507ab9206bb17d02f8238b553ef0c3e272834f
Rodrigo Pereira Silva <deveroodri@gmail.com>
2021-05-23 01:20:07 -0300

    Integração api google maps

---
+++ resources/views/admin/monitoramento/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+    <div id="map"></div>
+</div>
+@endsection
+
+@section('js')
+
+    <!-- Google Maps JS -->
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+    <script
+      src="https://maps.googleapis.com/maps/api/js?key={{env('API_KEY_GOOGLE')}}&callback=initMap&libraries=&v=weekly"
+      async
+    ></script>
+
+@endsection
