0cb8480995ce5de5272ae637db331c50f71b872a
Rodrigo Pereira Silva <deveroodri@gmail.com>
2021-05-23 00:22:37 -0300

    Login - admin

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,19 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request)
+    {
+        
+        $credencial = $request->only('email', 'password');
+        
+        if(!Auth::attempt($credencial, true))
+        {
+            return redirect('/login');
+        }
+        
+        return redirect($this->redirectTo);
+
     }
 }
